4001cfa2efcdaeb0b90397b4f08894834a9f3f01
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-10-27 21:40:02 -0600

    fixing eslint errors

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", function(req, res) {
-    res.render("index");
+router.get("/", (req, res) => {
+  res.render("index");
 });
 
 module.exports = router;

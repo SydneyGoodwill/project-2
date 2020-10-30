10f338e074921dc77999c01845255b6e25d5887b
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-29 19:00:53 -0600

    CREATED A DB! YAYA! (added db into routes.js)

--- models/amalgamate.js
+++ models/amalgamate.js
@@ -1,8 +1,10 @@
+/* eslint-disable prettier/prettier */
 module.exports = function (sequelize, DataTypes) {
   const Authenticate = sequelize.define("authenticate", {
     email: DataTypes.STRING,
     username: DataTypes.STRING,
     password: DataTypes.STRING,
   });
+  Authenticate.sync();
   return Authenticate;
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,15 @@
 const express = require("express");
-
+const db = require("../models")
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/register", async (req, res) => {
+router.get("/register", (req, res) => {
   res.render("register");
 });
+
+// router.post("/resgister")
 
 module.exports = router;

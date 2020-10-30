295962ee64b7083ffd28aa83e64e60143e305320
Zach LaFleur <zach.lafleur95@gmail.com>
2020-10-29 19:59:09 -0600

    changed table name, attempted to get a post request from register.. didnt work...

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("../models");
 const router = express.Router();
 
+
 router.get("/", (req, res) => {
   res.render("index");
   db;
@@ -11,6 +12,18 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
-// router.post("/resgister")
+router.post("/register", async (req, res) => {
+  try {
+    console.log(req.body)
+    await db.User.create(req.body);
+    res.redirect("/login");
+  } catch (err) {
+    res.status(500)
+  }
+})
+
+router.get("/login", (req, res) => {
+  res.render("login")
+})
 
 module.exports = router;

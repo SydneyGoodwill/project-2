5861f0abb9507f64895450c51b6bd446ff217133
nggerber <gerberclimbs@gmail.com>
2020-11-03 22:01:24 -0700

    removed var

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const passport = require("./passport-config");
 const flash = require("express-flash");
 const expressSession = require("express-session");
-const db = require("./models");
 
 // Requiring passport as we've configured it
 

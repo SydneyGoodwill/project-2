1f47cf361a24e406d16308d2778f819fec78c3ab
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-11-05 15:44:31 -0700

    trying again: adding travel, gamesearch, and routes.js updates - eslint/travis please work!

--- routes/routes.js
+++ routes/routes.js
@@ -5,121 +5,128 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", checkAuthenticated, (req, res) => {
-    res.render("index");
+  res.render("index");
 });
 
 router.get("/profile", checkAuthenticated, (req, res) => {
-    res.render("profile");
+  res.render("profile");
 });
 
 router.get("/games", checkAuthenticated, (req, res) => {
-    res.render("games");
-    db;
+  res.render("games");
+  db;
 });
 
 router.get("/games/snake", checkNotAuthenticated, (req, res) => {
-    res.render("snake");
+  res.render("snake");
 });
 
 router.get("/register", async(req, res) => {
-    const errors = await req.flash("error");
-    res.render("register", {
-        errors,
-    });
+  const errors = await req.flash("error");
+  res.render("register", {
+    errors,
+  });
 });
 
 router.post("/register", async(req, res) => {
-    try {
-        console.log("==========================");
-        console.log("POST ", req.body);
-        const user = await db.User.findOne({
-            where: { username: req.body.username },
-        });
-        const email = await db.User.findOne({ where: { email: req.body.email } });
-        console.log(user);
-        console.log(email);
+  try {
+    console.log("==========================");
+    console.log("POST ", req.body);
+    const user = await db.User.findOne({
+      where: { username: req.body.username },
+    });
+    const email = await db.User.findOne({ where: { email: req.body.email } });
+    console.log(user);
+    console.log(email);
 
-        if (user) {
-            req.flash("error", "that username already exists");
-            return res.redirect("/register");
-        }
-        if (email) {
-            req.flash("error", "that email is already registered");
-            return res.redirect("/register");
-        }
-        await db.User.create(req.body);
-        res.sendStatus(200);
-    } catch (err) {
-        console.log(err);
-        res.sendStatus(500);
+    if (user) {
+      req.flash("error", "that username already exists");
+      return res.redirect("/register");
     }
+    if (email) {
+      req.flash("error", "that email is already registered");
+      return res.redirect("/register");
+    }
+    await db.User.create(req.body);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 router.get("/login", checkNotAuthenticated, (req, res) => {
-    const errors = req.flash("error");
-    res.render("login", {
-        errors,
-    });
+  const errors = req.flash("error");
+  res.render("login", {
+    errors,
+  });
 });
 
 router.post(
-    "/login",
-    passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/login",
-        failureFlash: true,
-    })
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
 );
 
 router.get("/travel", (req, res) => {
-    res.render("travel");
+  res.render("travel");
 });
 
 router.post("/travel", async(req, res) => {
-    try {
-        const country = await req.body.searchcountry;
-        console.log(country);
-        var options = {
-            method: 'GET',
-            url: `https://rapidapi.p.rapidapi.com/name/` + country,
-            headers: {
-                'x-rapidapi-key': '58227b4c01msh31c608e79f4eccep1702fajsn305d3ba7aa2e',
-                'x-rapidapi-host': 'restcountries-v1.p.rapidapi.com'
-            }
-        };
+  try {
+    const country = await req.body.searchcountry;
+    console.log(country);
+    const options = {
+      method: "GET",
+      url: "https://rapidapi.p.rapidapi.com/name/" + country,
+      headers: {
+        "x-rapidapi-key": "58227b4c01msh31c608e79f4eccep1702fajsn305d3ba7aa2e",
+        "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+      },
+    };
 
-        axios.request(options).then(function(response) {
-            console.log(response.data);
-            res.render("travel", { news: response.data });
-        }).catch(function(error) {
-            console.error(error);
-        });
-        console.log(country);
-    } catch (err) { console.log(err) }
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+        res.render("travel", { news: response.data });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log(country);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.get("/gamesearch", (req, res) => {
-    res.render("gamesearch");
+  res.render("gamesearch");
 });
 
 router.post("/gamesearch", async(req, res) => {
-    try {
-        const game = await req.body.searchgame;
-        console.log(game);
-        var options = {
-            method: 'GET',
-            url: `https://rapidapi.p.rapidapi.com/games/` + game,
-            headers: {
-                'x-rapidapi-key': '58227b4c01msh31c608e79f4eccep1702fajsn305d3ba7aa2e',
-                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com'
-            }
-        };
+  try {
+    const game = await req.body.searchgame;
+    console.log(game);
+    const options = {
+      method: "GET",
+      url: "https://rapidapi.p.rapidapi.com/games/" + game,
+      headers: {
+        "x-rapidapi-key": "58227b4c01msh31c608e79f4eccep1702fajsn305d3ba7aa2e",
+        "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+      },
+    };
 
-        axios.request(options).then(function(response) {
-            console.log(response.data.name);
-            console.log(response.data.description_raw);
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data.name);
+        console.log(response.data.description_raw);
 
-            var card = `
+        const card = `
                 <div>
                 <div class="col-sm-6" style="margin-top: 0.9%;">
                   <div class="card" style="opacity: 0.8;">
@@ -131,29 +138,30 @@ router.post("/gamesearch", async(req, res) => {
                 </div>
               </div>`;
 
-
-            res.render.innerHTML = card;
-            res.render("gamesearch", { card });
-        }).catch(function(error) {
-            console.error(error);
-        });
-        console.log(game);
-    } catch (err) { console.log(err) }
+        res.render.innerHTML = card;
+        res.render("gamesearch", { card });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log(game);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-
 function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
 function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect("/");
-    }
-    next();
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
 }
 
 module.exports = router;
